611552697e2ae83d20292a5787b40cd87303220b
Vaish24navi <vaishnavimr2403@gmail.com>
2024-06-14 21:53:34 +0530

    debug(git): trying git-config unset

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
-
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 app = FastAPI(
     title="TallyTale",
     description="Polling through your next adventure.",
     version="0.1.0"
 )
 
+load_dotenv()
+MONGO_URL = os.getenv('MONGO_URI')
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(MONGO_URL, uuidRepresentation="standard")
+    database = app.mongodb_client["TallyTales"]
+    app.tales_collection = database.get_collection("TallyTale")
+    server_info = app.mongodb_client.server_info()
+    print(f"Connected to MongoDB server {server_info['version']}")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to TallyTale!"}
-
 
