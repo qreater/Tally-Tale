c6d44c07892e008e30c8e23d53ac484e2faf7d88
Niyas Hameed <niyasrad@gmail.com>
2024-07-02 09:28:39 +0530

    fix(frontend): layouting fix for responsiveness (#6)

--- frontend/src/containers/landing/landing.styles.ts
+++ frontend/src/containers/landing/landing.styles.ts
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export const LandingWrapper = styled.div`
     width: 100%;
     height: 100%;
-    max-width: 50rem;
+    max-width: 60rem;
     
     display: grid;
     gap: 2rem;
 
     margin: auto;
-    padding: 2rem;
+    padding: 1rem 0;
+    padding-bottom: 2rem;
     box-sizing: border-box;
 
     background-color: var(--bg-secondary-color);
@@ -27,11 +28,19 @@ export const LandingHeader = styled.div`
 export const LandingImg = styled.img`
     max-width: 50%;
     height: auto;
+
+    @media only screen and (max-height: 780px) {
+        max-width: 35%;
+    }
+    @media only screen and (max-width: 500px) {
+        max-width: 50%;
+    }
 `
 
 export const LandingTaglines = styled.div`
     text-align: end;
     display: flex;
+    max-width: 40%;
 
     flex-direction: column;
     gap: 1rem;
