61417210b3978513f526945f66e80ca81e35bdf7
Vaish24navi <vaishnavimr2403@gmail.com>
2025-01-20 11:52:55 +0530

    feat(backend): Addition of Follow and Unfollow API Feature

--- backend/core/model/user.py
+++ backend/core/model/user.py
@@ -5,11 +5,21 @@
 """
 
 
-from sqlalchemy import Column, String
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Index
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 from uuid import uuid4
 
 from core.utils.database import Base
+
+followers_association = Table(
+    "followers_association",
+    Base.metadata,
+    Column("follower_id", UUID(as_uuid=True), ForeignKey("user.id")),
+    Column("following_id", UUID(as_uuid=True), ForeignKey("user.id")),
+    Index("ix_follower_id", "follower_id"),
+    Index("ix_following_id", "following_id"),
+)
 
 
 class User(Base):
@@ -40,3 +50,14 @@ class User(Base):
     password = Column(String)
     description = Column(String, default=f"Hi, I'm {username}!")
     avatar_url = Column(String, default="./doc-assets/image.png")
+
+    followers_count = Column(Integer, default=0)
+    following_count = Column(Integer, default=0)
+
+    followers = relationship(
+        "User",
+        secondary=followers_association,
+        primaryjoin=id == followers_association.c.following_id,
+        secondaryjoin=id == followers_association.c.follower_id,
+        backref="following",
+    )
